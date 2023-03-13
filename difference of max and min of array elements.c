a7a71a0d263662650138263e81bb20d6de695285
jhansi5268 <125520142+jhansi5268@users.noreply.github.com>
2023-03-13 22:23:49 +0530

    Add files via upload

---
+++ difference of max and min of array elements.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+    int a[10],n,i,max,min;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+   max=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]>max)
+            max=a[i];
+    }
+    min=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]<min)
+            min=a[i];
+    }
+    printf("%d",max-min);
+
+}
